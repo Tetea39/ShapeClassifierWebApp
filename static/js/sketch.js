5cd8276ad511d2b07ea26a7c793ffb74525c3bfc
Hruaizela <48747390+Tetea39@users.noreply.github.com>
2020-12-06 02:52:53 +0530

    Back camera Test

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,50 +1,50 @@
-let shapeClassifier;
-let canvas;
-let resultDiv;
-let video;
+// let shapeClassifier;
+// let canvas;
+// let resultDiv;
+// let video;
 
-function setup(){
-    canvas = createCanvas(400, 400);
-    video = createCapture(VIDEO);
-    video.size(64,64);
-    background(255);
+// function setup(){
+//     canvas = createCanvas(400, 400);
+//     video = createCapture(VIDEO);
+//     video.size(64,64);
+//     background(255);
 
-    let options = {
-        inputs: [64, 64, 4],
-        task: "imageClassification",
-    };
-    shapeClassifier = ml5.neuralNetwork(options);
+//     let options = {
+//         inputs: [64, 64, 4],
+//         task: "imageClassification",
+//     };
+//     shapeClassifier = ml5.neuralNetwork(options);
 
-    const modelData = {
-        model: "{{ url_for('model', filename='model.json",
-        metadata: "{{ url_for('model', filename='model_meta.json')}}",
-        weights: "{{ url_for('model', filename='model.weights.bin')}}"
-    }
+//     const modelData = {
+//         model: "{{ url_for('model', filename='model.json",
+//         metadata: "{{ url_for('model', filename='model_meta.json')}}",
+//         weights: "{{ url_for('model', filename='model.weights.bin')}}"
+//     }
 
-    shapeClassifier.load(modelData, modelLoaded);
-    resultDiv = createDiv('loading Model');
-}
+//     shapeClassifier.load(modelData, modelLoaded);
+//     resultDiv = createDiv('loading Model');
+// }
 
-function classifyImage(){
-    shapeClassifier.classify({ image: video}, gotResults);
-}
+// function classifyImage(){
+//     shapeClassifier.classify({ image: video}, gotResults);
+// }
 
-function modelLoaded(){
-    console.log("Model Ready!!");
-    classifyImage();
-}
+// function modelLoaded(){
+//     console.log("Model Ready!!");
+//     classifyImage();
+// }
 
-function gotResults(err, results){
-    if(err){
-        console.error(err);
-        return;
-    }
-    let label = results[0].label;
-    let confidence = nf(100*results[0].confidence, 2, 0);
-    resultDiv.html(`${label} ${confidence}%`);
-    classifyImage();
-}
+// function gotResults(err, results){
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     let label = results[0].label;
+//     let confidence = nf(100*results[0].confidence, 2, 0);
+//     resultDiv.html(`${label} ${confidence}%`);
+//     classifyImage();
+// }
 
-function draw(){
-    image(video, 0, 0, width, height);
-}
+// function draw(){
+//     image(video, 0, 0, width, height);
+// }
